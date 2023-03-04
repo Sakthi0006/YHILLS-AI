a729eec3cd293a3656424cbe27d74fa3a1e88931
Sakthi0006 <126688768+Sakthi0006@users.noreply.github.com>
2023-03-04 16:33:33 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+#workking out in session
+#dictionary
+
+sample_dict={1:"apple",2:35,3:"data",2:"every",6:"35.4",7:35.4}
+sample_dict
+    
+  
+type(sample_dict)
+
+
+sample_dict[7]
+
+sample_dict["apple"]
+
+
+sample_dict
+
+sample_dict[10]="Business"
+
+sample_dict
